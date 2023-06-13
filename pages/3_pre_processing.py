77080de085feffa4c23f7bb4b209b5469d01effc
Nishiv Singh <singh.136@iitj.ac.in>
2023-06-13 17:05:55 +0530

    added the table for missing values

--- pages/3_pre_processing.py
+++ pages/3_pre_processing.py
@@ -2,24 +2,39 @@ import streamlit as st
 from streamlit import session_state
 
 def show_missing_info(df):
+    st.write("<h6> Missing values information for the dataset </h6>",unsafe_allow_html=True)
     null_text = df.isnull().any()
-    null_dict = {'column': [], 'contains null value': []}
+    null_val = df.isnull().sum()
+    null_dict = {'column': [], 'contains missing value': [], 'count of missing values':[]}
     for i in range(len(df.columns)):
         null_dict['column'].append(df.columns[i])
         if (null_text[i]):
-            null_dict['contains null value'].append("True")
+            null_dict['contains missing value'].append("True")
 
         else:
-            null_dict['contains null value'].append("False")
-    st.dataframe(null_dict,use_container_width=True)
+            null_dict['contains missing value'].append("False")
+        null_dict['count of missing values'].append(null_val[i])
+    st.dataframe(null_dict)
+    contains_null = df.isnull().values.any()
+    col_names = []
+    if contains_null:
+        for i in range(len(df.columns)):
+            if null_text[i]:
+                col_names.append(df.columns[i])
+    return col_names
 
 def show_pre_processing():
     st.write("<h1 style = 'text-align : center';> Pre-processing of data </h1>", unsafe_allow_html= True)
     st.write("---")
     st.write("<h4> Handle missing values </h4>",unsafe_allow_html=True)
-    show_missing_info(df_train)
-    st.write("<h6> Select a method </h6>",unsafe_allow_html=True)
-    missing_text = st.selectbox("Select",["Delete columns having > 75% missing data", "Filling the missing data by mean of the column"],label_visibility="collapsed")
+    col_names = show_missing_info(df_train)
+    if (len(col_names) == 0):
+        st.write("The data does not have any missing values !")
+    else:
+        st.write("<h6> Choose columns </h6>",unsafe_allow_html=True)
+        missing_columns = st.selectbox("Select",col_names,label_visibility="collapsed")
+        st.write("<h6> Select a method </h6>",unsafe_allow_html=True)
+        missing_text = st.selectbox("Select",["Delete columns having > 75% missing data", "Filling the missing data by mean of the column"],label_visibility="collapsed")
     st.write("---")
 
     st.write(" <h4> Encode categorical features </h4> " ,unsafe_allow_html=True)
