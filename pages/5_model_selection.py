a2f2950e1009a4f2bd6023559e7aa2841cbf45ea
Nishiv Singh <singh.136@iitj.ac.in>
2023-06-15 16:10:55 +0530

    Added the logic for changing the hyper-parameters as per the selected model

--- pages/5_model_selection.py
+++ pages/5_model_selection.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import streamlit as st
 from streamlit import session_state
 from streamlit_extras.add_vertical_space import add_vertical_space
@@ -5,16 +6,20 @@ from streamlit_extras.add_vertical_space import add_vertical_space
 
 def parameters(model):
     params_text = dict()
+    options_dict = dict()
     if model == "Support Vector Machine":
         params_text = {'kernal': 'rbf', 'degree': 3, 'gamma': 'scale', 'coef0': 0.0, 'tol': 0.001,
                        'C': 1.0, 'epsilon': 0.1, 'shrinking': True, 'cache_size': 200, 'verbose': False, 'max_iter': -1}
+        options_dict = {'kernal' : ['linear', 'poly', 'rbf', 'sigmoid', 'precomputed'],'gamma':['scale', 'auto']}
     elif model == "Linear Regression":
         params_text = {'fit_intercept': True, 'copy_X': True,
                        'n_jobs': None, 'positive': False}
+        options_dict = {}
     elif model == "Logistic Regression":
         params_text = {'penalty': 'l2', 'dual': False, 'tol': 0.0001, 'C': 1.0, 'fit_intercept': True, 'intercept_scaling': 1, 'class_weight': None,
                        'random_state': None, 'solver': 'lbfgs', 'max_iter': 100, 'multi_class': 'auto', 'verbose': 0, 'warm_start': False, 'n_jobs': None, 'l1_ratio': None}
-    return params_text
+        options_dict = {'penalty' : ['l1', 'l2', 'elasticnet', None],'solver':['lbfgs', 'liblinear', 'newton-cg', 'newton-cholesky', 'sag', 'saga'],'multi_class':['auto', 'ovr', 'multinomial']}
+    return params_text,options_dict
 
 
 def show_feature_selection():
@@ -45,7 +50,24 @@ def show_feature_selection():
     st.write("---")
 
     st.write("<h4> Set hyper-parameters </h4>", unsafe_allow_html=True)
-    model_params = parameters(model_text)
+    model_params,option_dict = parameters(model_text)
+
+    keys = list(model_params.keys())
+    values = list(model_params.values())
+
+    for i in range(len(keys)):
+        col1,col2, col3 = st.columns([10,1,10])
+        col1.text_input("Key", keys[i], key=f"key_input_{i}",label_visibility="collapsed",disabled=True)
+        value = values[i]
+        col2.text_input(":","=",key=i, label_visibility="collapsed",disabled=True)
+        if isinstance(value, str):
+            new_value = col3.selectbox("Value", options=option_dict[keys[i]], key=f"value_input_{i}",label_visibility="collapsed")
+        elif isinstance(value, bool):
+            new_value = col3.checkbox("Value", value=value, key=f"value_input_{i}",label_visibility="collapsed")
+        else:
+            new_value = col3.text_input("Value", value, key=f"value_input_{i}",label_visibility="collapsed")
+    session_state.model_params = model_params
+    st.write(session_state.model_params)
     st.write("---")
     st.write("<h4> Additional operations </h4>", unsafe_allow_html=True)
     st.write("---")
