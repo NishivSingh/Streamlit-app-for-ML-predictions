658ca23af6a2401601275685d7af622ca23b379a
Nishiv Singh <singh.136@iitj.ac.in>
2023-06-27 15:46:32 +0530

    chnaged some settings

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
-# Set the app layout to wide mode
-st.set_page_config(layout="wide")
+# # Set the app layout to wide mode
+# st.set_page_config(layout="wide")
 
 def main():
     st.write("<h1 style = 'text-align : center';> ML Predictor </h1> ", unsafe_allow_html=True)
