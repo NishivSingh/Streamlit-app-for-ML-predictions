fa39709bd3bf1ed4c198c2c99843c0d693c42491
Nishiv Singh <singh.136@iitj.ac.in>
2023-07-04 23:24:59 +0530

    displaying uploaded and predicted data

--- pages/8_prediction.py
+++ pages/8_prediction.py
@@ -19,23 +19,36 @@ def show_results():
     # Testing data
     testing_data_file_name = st.file_uploader(
         "**Upload the testing data**", ["csv"])
+
     if testing_data_file_name is not None:
         df_test = pd.read_csv(testing_data_file_name)
+        st.write("<h5 style = 'text-align : center;'> Test data </h5>",
+                 unsafe_allow_html=True)
+        st.dataframe(df_test)
+        # Make predictions
+        predict_btn = st.button("Predict the target values")
 
-        modified_df_test = df_test
-        if "scaler" in session_state:
-            modified_df_test = session_state.scaler.transform(df_test)
+        st.write("---")
+        if predict_btn:
+            modified_df_test = df_test
+            if "scaler" in session_state:
+                modified_df_test = session_state.scaler.transform(df_test)
 
-        session_state.model.fit(session_state.final_X_train,
-                                np.ravel(session_state.y_train))
-        predictions = session_state.model.predict(modified_df_test)
-        final_data = df_test
-        final_data.insert(
-            0, f"Predicted {session_state.targets}", predictions, True)
+            session_state.final_model.fit(session_state.final_X_train,
+                                          np.ravel(session_state.y_train))
+            predictions = session_state.final_model.predict(modified_df_test)
+            final_data = df_test
+            final_data.insert(
+                0, f"Predicted {session_state.targets}", predictions, True)
+            session_state.final_data = final_data
 
-        st.dataframe(final_data)
-        st.download_button(label="Download predictions", data=convert_df_to_csv(
-            final_data), file_name="predictions.csv", mime="text/csv")
+        if "final_data" in session_state:
+            st.write("<h5 style = 'text-align : center;'> Predicted data </h5>",
+                     unsafe_allow_html=True)
+            st.dataframe(session_state.final_data)
+            st.download_button(label="Download predictions", data=convert_df_to_csv(
+                session_state.final_data), file_name="predictions.csv", mime="text/csv")
+            st.write("---")
 
 
 if ("df_train" not in session_state):
